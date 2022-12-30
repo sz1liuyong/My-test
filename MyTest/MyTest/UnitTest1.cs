ec2cb4b998ab4b58a672eca60eeeb6eb1cb6a3e4
sz1li <sz1li@DELL-7590>
2022-12-30 15:09:58 +0800

    test submit

---
+++ MyTest/MyTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
